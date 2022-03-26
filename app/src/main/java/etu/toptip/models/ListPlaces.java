12407740741f7467f1b8534a6010cad57d712bf8
BEN AISSA Nadim <80357350+benaissanadim@users.noreply.github.com>
2022-03-26 23:52:46 +0100

    add map component

--- app/src/main/java/etu/toptip/models/ListPlaces.java
+++ app/src/main/java/etu/toptip/models/ListPlaces.java
@@ -1,13 +1,13 @@
-package etu.demoihm.maps.models;
+package etu.toptip.models;
 
 import com.google.android.gms.maps.model.LatLng;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
-import etu.demoihm.maps.R;
+import etu.toptip.R;
+
 
 public class ListPlaces {
 
@@ -17,13 +17,11 @@ public class ListPlaces {
 
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         listPlaces.clear();
-
         try {
-            listPlaces.add(new Place("Casino", "Supermarché", format.parse("2019-05-15"), R.drawable.img, "Nice", "Super promo tout à 50 %", new LatLng(43.583096, 7.124432)));
-            listPlaces.add(new Place("Carrefour", "Supermarché", format.parse("2019-05-15"), R.drawable.img, "Nice", "Super promo tout à 50 %", new LatLng(43.580111, 7.083920)));
-            listPlaces.add(new Place("Lidl", "Supermarché", format.parse("2019-05-15"), R.drawable.img, "Nice", "Super promo tout à 50 %", new LatLng(43.569384, 7.082547)));
-            listPlaces.add(new Place("Boucherie", "Boucherie", format.parse("2019-05-12"), R.drawable.img, "Antibes", "1kg de viande gratuit !!!", new LatLng(43.588567, 7.094391)));
-
+            listPlaces.add(new Place("Casino", "Supermarché", format.parse("2022-03-26"), R.drawable.img, "Antibes", "Super promo tout à 50 %", new LatLng(43.583096, 7.124432)));
+            listPlaces.add(new Place("Carrefour", "Supermarché", format.parse("2022-03-26"), R.drawable.img, "Antibes", "Super promo tout à 50 %", new LatLng(43.580111, 7.083920)));
+            listPlaces.add(new Place("Lidl", "Supermarché", format.parse("2022-03-26"), R.drawable.img, "Cannes", "Super promo tout à 50 %", new LatLng(43.569384, 7.082547)));
+            listPlaces.add(new Place("Boucherie", "Boucherie", format.parse("2022-03-26"), R.drawable.img, "Antibes", "1kg de viande gratuit !!!", new LatLng(43.588567, 7.094391)));
         } catch (ParseException e) {
             e.printStackTrace();
         }
--- app/src/main/java/etu/toptip/models/Place.java
+++ app/src/main/java/etu/toptip/models/Place.java
@@ -1,33 +1,22 @@
-package etu.demoihm.maps.models;
-
-import android.os.Parcel;
-import android.os.Parcelable;
-import android.util.Log;
+package etu.toptip.models;
 
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.Date;
-import java.util.Observable;
 
-import etu.demoihm.maps.R;
 
 public class Place {
 
     private String name;
-    private Integer quantiy;
     private String type;
     private Date date;
     private Integer image;
-    private String localisation; // pour les produits de la Boutique
+    private String localisation;
     private String description;
     private LatLng positiongps;
 
 
-    public Place() {
-        image = R.drawable.img;
-    }
 
-    // constructeur pour la Listvente, ListBoutique, ListReservation
 
     public Place(String name, String type, Date date, Integer image, String localisation, String description, LatLng positiongps) {
         this.name = name;
@@ -43,9 +32,6 @@ public class Place {
         return name;
     }
 
-    public Integer getQuantiy() {
-        return quantiy;
-    }
 
     public String getType() {
         return type;
